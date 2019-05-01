cd218ea1759237f8cf78db920e3be7b1e85db620
Joene Floresca <joenefloresca@gmail.com>
2019-05-02 07:55:41 +0800

    Added env for local development, Added screenshots, Added service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,7 @@ $app->singleton(
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 
 
 
